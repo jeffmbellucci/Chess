63f2cb27dac52bde965a2cd891ff8fa08f77c2fa
Jeff Belucci and Matt Purdy <student@appacademy.io>
2013-07-23 13:58:12 -0700

    Added all pieces

--- chess_pieces.rb
+++ chess_pieces.rb
@@ -31,7 +31,6 @@ class Piece
 end
 
 class Bishop < Piece
-
   def initialize(pos, color)
     super(pos, color, :bishop)
   end
@@ -42,4 +41,57 @@ class Bishop < Piece
   end
 end
 
-puts Bishop.new(1, :green).to_s.colorize(:red)
+class King < Piece
+  def initialize(pos, color)
+    super(pos, color, :king)
+  end
+
+  def move(end_pos)
+    #check for valid bishop move
+    # raise error if invalid
+  end
+end
+
+class Knight < Piece
+  def initialize(pos, color)
+    super(pos, color, :knight)
+  end
+
+  def move(end_pos)
+    #check for valid bishop move
+    # raise error if invalid
+  end
+end
+
+class Pawn < Piece
+  def initialize(pos, color)
+    super(pos, color, :pawn)
+  end
+
+  def move(end_pos)
+    #check for valid bishop move
+    # raise error if invalid
+  end
+end
+
+class Queen < Piece
+  def initialize(pos, color)
+    super(pos, color, :queen)
+  end
+
+  def move(end_pos)
+    #check for valid bishop move
+    # raise error if invalid
+  end
+end
+
+class Rook < Piece
+  def initialize(pos, color)
+    super(pos, color, :rook)
+  end
+
+  def move(end_pos)
+    #check for valid bishop move
+    # raise error if invalid
+  end
+end
